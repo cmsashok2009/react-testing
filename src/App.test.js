1764a7ff0ba8046851313f4231b6f80bdb024726
ashok.kumar086 <ashokkumar.natarajan@samrantech.com>
2022-03-06 18:06:55 +0530

    ejected

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("sample test", () => {
+  const wrapper = shallow(<App />);
+  //console.log(wrapper.debug());
+
+  expect(wrapper.length).toEqual(1);
+  expect(wrapper.exists()).toBe(true);
 });
